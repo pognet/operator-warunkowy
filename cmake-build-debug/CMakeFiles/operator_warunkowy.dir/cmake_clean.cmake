4ee93f54ef487b43e93bafbd1d9ca9755d58cc70
pognet <pogo@interbit.pl>
2018-10-12 16:05:25 +0200

    operator warunkowy

---
+++ cmake-build-debug/CMakeFiles/operator_warunkowy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/operator_warunkowy.dir/main.c.o"
+  "operator_warunkowy.pdb"
+  "operator_warunkowy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/operator_warunkowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
